73619efa7cd48802991d12689be638615162a212
halsoo <noizyufo@gmail.com>
2024-03-19 23:53:03 +0900

    fix: prevent errors on pyenv install

--- install_pyenv.sh
+++ install_pyenv.sh
@@ -1,3 +1,4 @@
+sudo apt-get install build-essential zlib1g-dev libffi-dev libssl-dev libbz2-dev libreadline-dev libsqlite3-dev liblzma-dev
 curl https://pyenv.run | bash
 echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.zshrc
 echo '[[ -d $PYENV_ROOT/bin ]] && export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.zshrc
